de203b2ca03919b7e77fb96e997da1eef2177e94
Zeonvillan <Zeonvillan@gmail.com>
2023-05-20 14:57:00 +0530

    Update

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello");
+﻿using Stack;
+using System;
+class Program
+{
+	static void Main(String[] args)
+	{
+		int choice, value;
+		StackDemo obj = new StackDemo();
+		Console.WriteLine("Enter the Size of Stack: ");
+		int s = Convert.ToInt32(Console.ReadLine());
+		obj.createStack(s);
+	}
+}
